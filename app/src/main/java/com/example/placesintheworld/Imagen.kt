fb3d89617cfef627be3db3e5a01efac62003f553
alu12722348 <aluolm2348@ieselcaminas.org>
2023-11-17 19:25:47 +0100

    .

--- app/src/main/java/com/example/placesintheworld/Imagen.kt
+++ app/src/main/java/com/example/placesintheworld/Imagen.kt
@@ -5,15 +5,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -23,6 +26,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.placesintheworld.ui.theme.Principal
 import com.example.placesintheworld.ui.theme.Salty
+import com.example.placesintheworld.ui.theme.Segundo
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,14 +34,18 @@ import com.example.placesintheworld.ui.theme.Salty
 fun Imagen(navControllerName:String, navControllerImagen: Int, navController: NavHostController){
     Column(
         Modifier
-            .fillMaxWidth()
-            .background(Principal),
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(
+                colors = listOf(
+                    Principal, Segundo
+                )
+            )),
     )
     {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxWidth().size(80.dp)
+                .fillMaxWidth()
         ){
             Text(
                 text = navControllerName,
@@ -45,14 +53,14 @@ fun Imagen(navControllerName:String, navControllerImagen: Int, navController: Na
                 color = Color.White,
                 fontFamily = Salty,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.weight(1f)
             )
         }
         Image(
             painter = painterResource(id = navControllerImagen),
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
